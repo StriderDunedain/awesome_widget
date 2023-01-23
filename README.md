881c80cfdbd5f8e23027336fc4650ae645fe6c8d
Максим Трухинов <maks.truxinov@yandex.ru>
2023-01-23 15:32:20 +0300

    Improved readability and other minor changes

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # A simple widget for eng-rus translations
 Initially a project for Computer Science class but who knows what it can become...
 This is a pre-alpha version so it's very cringe!
+If you want to take part in this project or contribute to it, please reach out to maks.truxinov@yandex.ru before forking or commiting to main branch
+Enjoy our product :)
+
+This project is licensed under the terms of the MIT license.
--- functions.py
+++ functions.py
@@ -1,18 +1,17 @@
 """
 -------->
-These functions get response from a database of words using SQLite queries.
+These functions get data from a database of words using SQLite queries.
 -------->
-All functions return a tuple like this: ('eng_word', 'rus_word')
 By default:
  - The database is called 'database.db'
  - The table is called 'words'
 -------->
 """
 
+import logging
 import sqlite3
 from sqlite3 import OperationalError
-import logging
-from sys import stdout
+from random import choice
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -22,7 +21,7 @@ logging.basicConfig(
 )
 
 logger = logging.getLogger(__name__)
-handler = logging.StreamHandler(stream=stdout)
+handler = logging.FileHandler(filename='logs.log')
 logger.addHandler(handler)
 
 conn = sqlite3.connect('database.db')
@@ -44,8 +43,7 @@ except OperationalError as error:
 def insert_word(eng_word=None, rus_word=None):
     """Adds new word-pair to database"""
     if eng_word is None or rus_word is None:
-        error = 'Incorrect args for `insert_word` func'
-        logger.error(error)
+        logger.error('Incorrect args for `insert_word` func')
         return None
 
     try:
@@ -58,58 +56,54 @@ def insert_word(eng_word=None, rus_word=None):
         logger.error(f'OperationalError while inserting new values to db: {e}')
 
 
-def get_pair_eng(eng_word=None):
-    """Returns a translation of given word (word=ENG)"""
+def rus_from_eng(eng_word=None):
+    """Returns Russian translation from English word"""
     if eng_word is None or eng_word == '':
-        error = 'Incorrect arg for `get_pair_eng` func'
-        logger.error(error)
+        logger.error('Incorrect arg for `rus_from_eng` func')
         return None
 
     try:
         data_sample = cur.execute(f"""
-                SELECT eng_word, rus_word
+                SELECT rus_word
                 FROM words
                 WHERE eng_word = '{eng_word}';
             """)
-        logger.info(f'Selected translation for {eng_word}')
 
-        result_tuple = [i for i in data_sample][0]
-        return result_tuple
+        russian_tuple = [i for i in data_sample][0]
+        russian_word = russian_tuple[0]
+        return russian_word
     except OperationalError as error:
         logger.error(f'OperationalError while getting values from db: {error}')
 
 
-def get_pair_rus(rus_word=None):
-    """Returns a translation of given word (word=RUS)"""
+def eng_from_rus(rus_word=None):
+    """Returns English translation from Russian word"""
     if rus_word is None or rus_word == '':
-        error = 'Incorrect arg for `get_pair_rus` func'
-        logger.error(error)
+        logger.error('Incorrect arg for `eng_from_rus` func')
         return None
 
     try:
         data_sample = cur.execute(f"""
-                SELECT eng_word, rus_word
+                SELECT eng_word
                 FROM words
                 WHERE rus_word = '{rus_word}';
             """)
-        logger.info(f'Selected translation for {rus_word}')
-
-        result_tuple = [i for i in data_sample][0]
-        return result_tuple
+        english_tuple = [i for i in data_sample][0]
+        english_word = english_tuple[0]
+        return english_word
     except OperationalError as error:
         logger.error(f'OperationalError while getting values from db: {error}')
 
 
 def get_all():
-    """Returns a translation of given word (word=ENG)"""
+    """Yields a tuple of eng:rus word pairs for all """
     try:
         result = cur.execute("""
                     SELECT *
                     FROM words;
                 """)
-        logger.info('Returned all')
-        final = [i for i in result]
-        return final
+        for i in result:
+            yield choice(result)
     except OperationalError as error:
         logger.error(f'OperationalError while getting values from db: {error}')
 
@@ -117,14 +111,12 @@ def get_all():
 def delete_record(eng_word=None):
     """Deletes a record from database. Arg is ONLY in English!"""
     if eng_word is None:
-        error = 'Not expected `eng_word` to equal None'
-        logger.error(error)
+        logger.error('Not expected `eng_word` to equal None')
         return None
     try:
         cur.execute(f"""
             DELETE FROM words WHERE eng_word = '{eng_word}';
         """)
-        logger.info('Word-pair successfully deleted from database')
     except OperationalError as error:
         logger.error(f'OperationalError while deleting from database: {error}')
 
